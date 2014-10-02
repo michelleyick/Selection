4e9e2f6dba9b08c12482b3bcba547e17a6d5afd7
41213 <41213@PC003438.coll.lr.local>
2014-10-02 11:47:08 +0100

    Completed selection class exercises

---
+++ Selection class exercises (revision Q2).py
@@ -0,0 +1,13 @@
+#Michelle Yick
+#1-10-2014
+#Selection class exercises
+
+#Revision Q2
+
+age = int(input("Please enter your age"))
+
+if age < 17:
+    print("You are not old enough to drive")
+
+elif age >= 17:
+    
---
+++ Selection class exercises task 1.py
@@ -0,0 +1,33 @@
+#Michelle Yick
+#01/10/2014
+#Selection class exercises part 1 of task 1
+
+month = int(input("Please enter a month as a number between 1-12: "))
+if month == 1:
+    print("The month you entered is January")
+if month == 2:
+    print("The month you entered is February")
+if month == 3:
+    print("The month you entered is March")
+if month == 4:
+    print("The month you entered is April")
+if month == 5:
+    print("The month you entered is May")
+if month == 6:
+    print("The month you entered is June")
+if month == 7:
+    print("The month you entered is July")
+if month == 8:
+    print("The month you entered is August")
+if month == 9:
+    print("The month you entered is September")
+if month == 10:
+    print("The month you entered is October")
+if month == 11:
+    print("The month you entered is November")
+if month == 12:
+    print("The month you entered is December")
+elif month >12:
+    print("Your number is to high")
+elif month <12:
+    print("Your number is to low")
