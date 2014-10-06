ea8f3c6872cc252461fd1c42167e7e351394e1c3
41213 <41213@PC003438.coll.lr.local>
2014-10-06 09:38:20 +0100

    # WARNING: head commit changed in the meantime
    completed selection class exercises

---
+++ class exercises (revision Q3).py
@@ -0,0 +1,19 @@
+#Michelle Yick
+#02-10-2014
+#Selection class exercises
+
+#Revision Q3
+
+number = int(input("Please enter a number between 21 and 29:"))
+
+if number < 21:
+    print("Your number is too low.")
+elif number > 29:
+    print("Your number is too high")
+elif number > 21:
+    print("Your number is in the range")
+elif number <29:
+    print("Your number is in the range")
+    
+    
+    
