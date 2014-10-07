4751ee6b3845fe26fe0fd8415a2bd07e46d7b881
Michelle Yick <41213@longroad.ac.uk>
2014-10-07 16:59:36 +0100

    # WARNING: head commit changed in the meantime
    Completed selection class exercise

---
+++ Class exercises Task 1.py
@@ -0,0 +1,33 @@
+#Michelle Yick
+#01-10-2014
+#Class exercises Task 1
+
+month = int(input("Please enter a month as a number between 1-12: "))
+if month == 1:
+    print("The month you entered is January")
+elif month == 2:
+    print("The month you entered is February")
+elif month == 3:
+    print("The month you entered is March")
+elif month == 4:
+    print("The month you entered is April")
+elif month == 5:
+    print("The month you entered is May")
+elif month == 6:
+    print("The month you entered is June")
+elif month == 7:
+    print("The month you entered is July")
+elif month == 8:
+    print("The month you entered is August")
+elif month == 9:
+    print("The month you entered is September")
+elif month == 10:
+    print("The month you entered is October")
+elif month == 11:
+    print("The month you entered is November")
+elif month == 12:
+    print("The month you entered is December")
+elif month < 1:
+    print("The number you have entered is too low")
+elif month > 12:
+    print("The number you have entered is too high")
