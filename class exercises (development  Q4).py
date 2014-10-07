8ea8d4cfeda14ad826eff80c0ce94bb8bd1f4665
41213 <41213@PC140028.coll.lr.local>
2014-10-07 11:06:32 +0100

    completed selection class exercises

---
+++ class exercises (development  Q4).py
@@ -0,0 +1,26 @@
+#Michelle Yick
+#06-10-2014
+#Selection class exercises development Q4
+
+mark = int(input("Please enter your exam mark:"))
+
+if mark > 81 and mark < 100:
+    print("You've achieved a grade A")
+
+elif mark > 71 and mark < 80:
+    print("You've achieved a grade B")
+
+elif mark > 61 and mark < 70:
+    print("You've achieved a grade C")
+
+elif mark > 51 and mark < 60:
+    print("You've achieved a grade D")
+
+elif mark > 41 and mark <50:
+    print("You've achieved a grade E")
+
+elif mark > 0 and mark < 40:
+    print("You've achieved a grade U")
+
+else:
+    print("You have entered an invalid number. Please enter a number between 0 and 100")
