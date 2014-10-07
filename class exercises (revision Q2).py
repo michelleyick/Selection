94031159e651b91bb5ad172eaa57e8f456a269d1
Michelle Yick <41213@longroad.ac.uk>
2014-10-07 18:39:29 +0100

    Theory class encoding exercise

---
+++ class exercises (revision Q2).py
@@ -0,0 +1,19 @@
+#Michelle Yick
+#1-10-2014
+#Selection class exercises
+
+#Revision Q2
+
+age = int(input("Please enter your age"))
+
+if age < 17:
+    print("You are not old enough to drive")
+
+if age > 17:
+    print("You are old enough to drive")
+
+if age == 17:
+    print("You are old enough to drive")
+    
+    
+    
