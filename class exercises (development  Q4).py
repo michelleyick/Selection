df02ea5c3ee667601d313e14321a47c4997f676f
41213 <41213@PC140028.coll.lr.local>
2014-10-07 11:16:46 +0100

    completed class exercises

--- class exercises (development  Q4).py
+++ class exercises (development  Q4).py
@@ -4,22 +4,22 @@
 
 mark = int(input("Please enter your exam mark:"))
 
-if mark > 81 and mark < 100:
+if mark > 81 and mark < 100 or mark == 100 or mark == 81:
     print("You've achieved a grade A")
 
-elif mark > 71 and mark < 80:
+elif mark > 71 and mark < 80 or mark == 71 or mark == 80:
     print("You've achieved a grade B")
 
-elif mark > 61 and mark < 70:
+elif mark > 61 and mark < 70 or mark == 61 or mark == 70:
     print("You've achieved a grade C")
 
-elif mark > 51 and mark < 60:
+elif mark > 51 and mark < 60 or mark == 51 or mark == 60:
     print("You've achieved a grade D")
 
-elif mark > 41 and mark <50:
+elif mark > 41 and mark < 50 or mark == 41 or mark == 50:
     print("You've achieved a grade E")
 
-elif mark > 0 and mark < 40:
+elif mark > 0 and mark < 40 or mark == 0 or mark == 40:
     print("You've achieved a grade U")
 
 else:
