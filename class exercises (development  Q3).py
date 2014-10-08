6e5e625b247a0860a2aeb8768fa1d1dbfeee546a
41213 <41213@PC004141.coll.lr.local>
2014-10-08 14:33:52 +0100

    completed selection class exercises

--- class exercises (development  Q3).py
+++ class exercises (development  Q3).py
@@ -1,14 +1,18 @@
 #Michelle Yick
-#06-10-2014
+#10-10-2014
 #Selection class exercises development Q3
 
 hours = int(input("Please enter the number of hours you work in one week:"))
 
-pay_rate = int(input("Please enter your hourly pay rate:"))
-
-if hours > 40:
-    print("You will get paid 1.5 times the rate due to extra hours")
-elif hours < 0 or hours > 60:
+if hours > 40 and hours < 60 or hours == 60:
+    pay_rate = int(input("Please enter your hourly pay rate:"))
+    answer = pay_rate*1.5
+    print("The amount of money you will recieve after the extra pay is {0}".format(answer))
+    
+elif hours < 40 or hours == 40:
+    print("Sorry you are not eligable for any extra pay.")
+    
+else:
     print("Sorry the number you have entered is not in the range")
 
 
